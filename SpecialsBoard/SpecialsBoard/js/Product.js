3c97b2cd66ad19e7e9789041a17917d8b5662cdf
Eposnow Developers <Developer@Eposnow.com>
2014-07-28 13:57:55 +0100

    Specials Board Initial Commit
    Initial commit of the specials board example App using Eposnow API V1

---
+++ SpecialsBoard/SpecialsBoard/js/Product.js
@@ -0,0 +1,154 @@
+﻿function Product() {
+    var self = this;
+
+    // Define URI routes for API
+    self.productURI = 'https://api.eposnowhq.com/v1/product/';
+    self.stockUIR = 'https://api.eposnowhq.com/api/v1/productstock/';
+
+    // Define your API access key
+    self.API_KEY = 'ENTER YOUR API KEY HERE';
+
+    // Array to hold all products from API
+    self.products = new Array();
+
+    // Array to hold all selected product IDs
+    self.selectedProductsIDs = new Array();
+
+    // Array to hold selected product objects
+    self.specialProducts = new Array();
+
+    // New instance of the SpecialsScreen - defined in SpecialScreen.js
+    var screen = new SpecialsScreen();
+
+    // Refresh rate for screen, i.e polling interval (In seconds)
+    self.screenRefreshRate = 2;
+
+    // Base method for calling the API itself
+    // Returns the response from the call
+    // ___Params___
+    // uri: The URI the request is going to i.e https://api.eposnowhq.com/v1/product/
+    // method: The request verb i.e POST, GET, PUT/PATCH, DELETE
+    // data: Any data to pass, must be of correct format, in this case JSON
+    self.ajax = function (uri, method, data) {
+        var request = {
+            url: uri,
+            type: method,
+            contentType: "application/json",
+            accepts: "application/json",
+            dataType: 'json',
+            data: JSON.stringify(data),
+            crossDomain: true,
+            beforeSend: function (xhr) {
+                //Sets Basic Authorization header necessary for API calls using key defined at top
+                xhr.setRequestHeader("Authorization", "Basic " + self.API_KEY);
+            },
+            error: function (jqXHR) {
+                console.log("ajax error " + jqXHR.status);
+            }
+        };
+        return $.ajax(request);
+    }
+
+    // Method to return the current stock level for each product selected on the board
+    self.getStockForProduct = function () {
+        // For all selected products
+        self.specialProducts.forEach(function (prod) {
+            // Request stock level from API
+            self.ajax(self.stockUIR + "?ProductID=" + prod.ProductID, "GET").done(function (data) {
+                if (data != null) {
+                    data.forEach(function (st) {
+                        // Update stock level from response
+                        prod.Stock = st.CurrentStock;
+                    });
+                }
+            });
+        });
+    }
+
+    // Method to get all products from the API and display them on the specials board for selection
+    self.getAllProducts = function () {
+        // Request all products from API
+        var req = self.ajax(self.productURI, "GET");
+        req.done(function (data) {
+            // Store all products
+            self.products = data;
+            // Display products on-screen
+            screen.displayProductList(data);
+        });
+        req.fail(function (jqXHR, textStatus) {
+            console.log("Request failed: " + textStatus);
+        });
+    }
+
+    // Method to store the productID and product of each selected product on the specials board
+    self.getProductSelection = function () {
+        // For each checked product in the product display
+        $("#products").find("input:checked").each(function (i, ob) {
+            // Store ID
+            self.selectedProductsIDs.push($(ob).val());
+        });
+        // For each of the stored product IDs
+        self.selectedProductsIDs.forEach(function (productID) {
+            // Store the product object itself
+            self.specialProducts.push(self.products.get(productID));
+        });
+    }
+
+    // Method to display selected products on special board
+    self.select = function () {
+        // Get selected productIDs
+        self.getProductSelection();
+        // Get product objects for those IDs
+        self.addPropertyToSelected();
+        // get stock for each selected product
+        self.getStockForProduct();
+        // Clear screen
+        screen.clearScreen();
+        // Hide selection button
+        screen.selectButton(false);
+        // Display selected products and stock
+        self.displaySelected();
+    }
+
+    self.displaySelected = function () {
+        // Display products on screen 
+        screen.displaySpecialProducts(self.specialProducts);
+        // Refresh stock and screen at defined intervals
+        self.refreshScreen();
+    }
+
+    // Method to add stock property to product object
+    self.addPropertyToSelected = function () {
+        // For each object of special product selected
+        self.specialProducts.forEach(function (prod) {
+            // Add stock property
+            prod["Stock"] = 0;
+        });
+    }
+
+    // Method to refresh stock levels and display on screen
+    self.refreshScreen = function () {
+        setInterval(function () {
+            // Get stock for each selected product
+            self.getStockForProduct();
+            // Display products on screen
+            screen.displaySpecialProducts(self.specialProducts);        
+        }, self.screenRefreshRate * 1000); // screenRefreshRate defined in seconds, times by 1000 as milliseconds required.
+    }
+}
+
+// Override for array get method
+// Returns the product by ID
+// ___Params___
+// pId: The ID of the product to return
+Array.prototype.get = function (pId) {
+    // For each product entry in the array
+    for (var i = 0, len = this.length; i < len; i++) {
+        // Ensure it is of correct type object
+        if (typeof this[i] != "object") continue;
+        // Return object from array that matches the given ID
+        if (this[i].ProductID == pId) {
+            return this[i];
+        }
+    }
+};
---
+++ SpecialsBoard/SpecialsBoard/js/SpecialScreen.js
@@ -0,0 +1,97 @@
+﻿function SpecialsScreen() {
+
+    var self = this;
+
+    // Define HTML elements
+    self.productsDiv = "#products";
+    self.selectButtonDiv = "selectButton";
+
+    // Method to display list of all products
+    // ___Params___
+    // products: Array of valid product objects to display
+    self.displayProductList = function (products) {
+        var pDiv = "";
+        // Build HTML from template for each product in array
+        products.forEach(function (prod) {
+            pDiv += ' <div class="row" >';
+            pDiv += '<div class="col-md-1"></div>';
+            pDiv += '<div class="col-md-1">' + prod.ProductID + '</div>';
+            pDiv += '<div class="col-md-1"><input type="checkbox" class="squaredTwo" name="" value="' + prod.ProductID + '"/></div>';
+            pDiv += '<div class="col-md-3">' + prod.Name + '</div>';
+            pDiv += '<div class="col-md-5">' + prod.Description + '</div>';
+            pDiv += '<div class="col-md-1">' + prod.SalePrice + '</div>';
+            pDiv += '</div>';
+        });
+        // Add complete HTML to product element
+        $(self.productsDiv).html(pDiv);
+    }
+
+    // Method to clear the screen
+    self.clearScreen = function () {
+        $(self.productsDiv).html('');
+    }
+
+    // Method to show or hide Selection Button
+    // ___Params___
+    // setVisible: boolean value to show or hide the select button
+    self.selectButton = function (setVisible) {
+        if (setVisible) {
+            document.getElementById(self.selectButtonDiv).style.visibility = "visible";
+        } else {
+            document.getElementById(self.selectButtonDiv).style.visibility = "hidden";
+        }
+    }
+
+    // Method to display selected special products
+    // ___Params___
+    // products: Array of valid product objects to display
+    self.displaySpecialProducts = function (products) {
+        var pDiv = "";
+        // Build HTML from template for each product in array
+        products.forEach(function (prod) {
+            pDiv += '<div class="row chalk_50 ">';
+            pDiv += '<div class="col-md-2"></div>';
+            // Change display if product is out of stock
+            if (prod.Stock == 0) {
+                pDiv += '<div class="col-md-6 empty_stock">' + prod.Name + '</div>';
+            } else {
+                pDiv += '<div class="col-md-6">' + prod.Name + '</div>';
+            }
+            pDiv += '<div class="col-md-1">£' + self.fixPriceDisplay(prod.SalePrice) + '</div>';
+            pDiv += '<div class="col-md-1"></div>';
+            pDiv += self.displayStock(prod.Stock);
+            pDiv += '</div>';
+        });
+        // Add complete HTML to product element
+        $(self.productsDiv).html(pDiv);
+    }
+
+    // Method to display double digits prices correctly
+    // ___Params___
+    // price: The price value to be displayed
+    self.fixPriceDisplay = function (price) {
+        return price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    // Method to return HTML representation of stock value
+    // ___Params___
+    // stock: The products stock value
+    self.displayStock = function (stock) {
+        var inStock = stock / 10;
+        // Only show maximum of 10 'tallies'
+        if (inStock > 10) inStock = 10;
+        var stockDisplay = "";
+        // Generate HTML for stock value
+        for (var i = 0; i < inStock; i++) {
+            stockDisplay += '/';
+        }
+        // For any 'tallies' to represent not in stock, generate out of stock 'tallies'
+        for (var i = 0; i <= 9 - inStock; i++) {
+            stockDisplay += '<span class="empty_stock">/</span>';
+        }
+        return stockDisplay;
+    }
+}
+
+
+
